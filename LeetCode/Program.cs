e4bae3ecfb37e6d1aa911ffda5061ea51247846d
John Kehinde <kehindejohn018@gmail.com>
2023-02-10 10:56:14 +0100

    solution

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -3,5 +3,5 @@
 using LeetCode;
 
 var solution = new Solution();
-foreach (var item in solution.TwoSum2(new[] { 2, 5, 5, 11 }, 10))
-Console.WriteLine(item);
+//foreach (var item in solution.TwoSum2(new[] { 2, 5, 5, 11 }, 10))
+Console.WriteLine(solution.Reverse(1534236469));
--- LeetCode/Solution.cs
+++ LeetCode/Solution.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace LeetCode
+﻿namespace LeetCode
 {
     public class Solution
     {
@@ -56,6 +54,69 @@ namespace LeetCode
             carry = total / 10;
 
             return new ListNode(total % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
+        }
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            var result = new HashSet<char>();
+            int previous = 0, next = 0, count = 0;
+            while (next < s.Length)
+            {
+                if (!result.Contains(s[next]))
+                {
+                    result.Add(s[next]);
+                    next++;
+                    count = Math.Max(count, result.Count);
+                }
+                else
+                {
+                    result.Remove(s[previous]);
+                    previous++;
+                }
+            }
+            return count;
+        }
+
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] nums3 = new int[nums1.Length + nums2.Length];
+            //Array.Copy(nums1, nums3, nums1.Length);
+            Array.Copy(nums1, 0, nums2, nums1.Length, nums2.Length);
+            Array.Sort(nums3);
+            var arrayLength = nums3.Length;
+
+            double median;
+
+            var isOdd = arrayLength % 2 != 0;
+            if (isOdd)
+            {
+                median = nums3[(arrayLength + 1) / 2 - 1];
+            }
+            else
+            {
+                median = (nums3[arrayLength / 2 - 1] + nums3[arrayLength / 2]) / 2.0d;
+            }
+            return median;
+
+        }
+
+        public string LongestPalindrome(string s)
+        {
+            var result = string.Empty;
+
+
+            return result;
+        }
+
+        public int Reverse(int x)
+        {
+            int reversed = 0;
+            while (Convert.ToBoolean(x))
+            {
+                reversed = reversed * 10 + (x % 10);
+                x = x / 10;
+            }
+            return reversed;
         }
     }
 
